d708c635734b154c16fb02d2393a3c761406e7fd
Enrico Shippole <enricoship@gmail.com>
2022-07-07 20:17:26 -0400

    Add more to the architecture

--- alibi/alibi.py
+++ alibi/alibi.py
@@ -7,6 +7,50 @@ from math import log2, floor
 def exists(val):
     return val is not None
 
+# residual wrapper
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(x, **kwargs) + x
+
+# pre-normalization wrapper
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+# gated-GELU activation function
+
+class GEGLU(nn.Module):
+    def forward(self, x):
+        x, gates = x.chunk(2, dim = -1)
+        return x * F.gelu(gates)
+
+# feedforward layer with gated-GELU activation function
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, mult = 4, dropout = 0.):
+        super().__init__()
+        inner_dim = int(dim * mult)
+        self.net = nn.Sequential(
+            nn.Linear(dim, inner_dim * 2),
+            GEGLU(),
+            nn.Dropout(dropout), # optional dropout
+            nn.Linear(inner_dim, dim)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
 # AliBi Positional Bias
 
 class AlibiPositionalBias(nn.Module):
@@ -52,3 +96,90 @@ class AlibiPositionalBias(nn.Module):
 
         return bias
 
+# attention
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        heads = 8,
+        dim_head = 64,
+        dropout = 0.
+    ):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        inner_dim = heads * dim_head
+
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_q = nn.Linear(dim, inner_dim, bias = False)
+        self.to_kv = nn.Linear(dim, dim_head * 2, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim)
+
+        self.rel_pos_bias = T5RelativePositionBias(scale = dim_head ** 0.5, heads = heads)
+
+    def forward(self, x):
+        h, device = self.heads, x.device
+
+        q, k, v = (self.to_q(x), *self.to_kv(x).chunk(2, dim = -1))
+
+        q = rearrange(q, 'b n (h d) -> b h n d', h = h)
+        q = q * self.scale
+
+        sim = einsum('b h i d, b j d -> b h i j', q, k)
+        i, j = sim.shape[-2:]
+
+        # T5 Relative Positional Bias
+        sim = self.rel_pos_bias(sim)
+
+        # Causal Mask
+        causal_mask = torch.ones((i, j), dtype = torch.bool, device = device).triu(j - i + 1)
+        sim = sim.masked_fill(causal_mask, -torch.finfo(sim.dtype).max)
+
+        attn = sim.softmax(dim = -1)
+
+        attn = self.dropout(attn) # Optional dropout
+
+        out = einsum('b h i j, b j d -> b h i d', attn, v)
+
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+# Transformer
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Residual(PreNorm(dim, Attention(dim = dim, heads = heads, dim_head = dim_head, dropout = dropout))),
+                Residual(PreNorm(dim, FeedForward(dim = dim, dropout = dropout)))
+            ]))
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x)
+            x = ff(x)
+        return x
+
+# LaMDA Model
+
+class LaMDA(nn.Module):
+    def __init__(self, *, num_tokens, dim, depth, dim_head, heads):
+        super().__init__()
+        self.token_emb = nn.Embedding(num_tokens, dim)
+
+        self.transformer = Transformer(dim, depth, dim_head, heads)
+
+        self.to_logits = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, num_tokens)
+        )
+
+    def forward(self, x):
+        x = self.token_emb(x)
+        x = self.transformer(x)
+        logits = self.to_logits(x)
+        return logits
